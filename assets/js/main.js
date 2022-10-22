7a9bfb0ddf745f106e32e385085c43a7ce37793b
Franz-SuperCode <franzsupercode@gmail.com>
2022-10-22 14:57:52 +0200

    Fertig

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
-// console.log("Hallo");
+let mobile_menu = document.getElementById("mobile_menu");
+// //Die Striche vom Menu verstecken
+// let getHeaderDiv = document.querySelector(".div_menu");
 
-// let burgerMenu = document.querySelector(".navigation");
-// burgerMenu.addEventListener("click", e => {
-//     burgerMenu.style.left = 0;
-//     burgerMenu.style.right = 0;
-// });
+function showMenu() {
+    console.log("show menu")
+    mobile_menu.style.left = "80%";
+    mobile_menu.style.right = 0;
+    // //Die Striche vom Menu verstecken
+    // getHeaderDiv.style.display = "none";
+
+}
+
+function closeMenu() {
+    console.log("close menu")
+    mobile_menu.style.left = "100%"
+    mobile_menu.style.right = "-100%"
+    // //Die Striche vom Menu wieder holen
+    // getHeaderDiv.style.display = "flex";
+}
